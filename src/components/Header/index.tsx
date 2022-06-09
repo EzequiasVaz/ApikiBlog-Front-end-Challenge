4b225eafa7d608da4abfc41018cdab705ed25bf6
EzequiasVaz <ezequiasvaz@hotmail.com>
2022-06-09 03:01:06 -0300

    mudando alguns pathname

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,9 @@ export function Header() {
     return (
         <header>
             <nav>
-                <Link to='/'><img loading="lazy" src="/src/assets/apikiBlogHeader.svg" alt="Logo da Apiki Blog" /></Link>
+                <Link to='/'><img loading="lazy" src="../../assets/apikiBlogHeader.svg" alt="Logo da Apiki Blog" /></Link>
                 <button className="button-newsletter">Cadastrar email</button>
-                <button className="menu-mobile hidden"><img src="/src/assets/List-menu-mobile.svg" alt="menu" /></button>
+                <button className="menu-mobile hidden"><img src="../../assets/List-menu-mobile.svg" alt="menu" /></button>
             </nav>
         </header>
     )
--- src/utils/apikiProfile.ts
+++ src/utils/apikiProfile.ts
@@ -1,8 +1,8 @@
 export const ApikiProfile = {
     id: Math.random(),
     name: 'Apiki Blog',
-    avatar: '/src/assets/apikiPictureProfile.svg',
-    postImage: '/src/assets/postImage.png',
+    avatar: '../assets/apikiPictureProfile.svg',
+    postImage: '../assets/postImage.png',
     postImageAlternativeText: 'Logo da Apiki',
     tempoDeLeitura: '5 minutos',
     data: '2018-01-27T11:00:00'
